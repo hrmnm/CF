454227d6b14bee3ff6d4b0a2f0bf96d5c2ac8a13
haramanami <manami.hara@cybermarketing>
2019-09-03 18:08:17 +0900

    responsive

--- cf.js
+++ cf.js
@@ -1,17 +1,13 @@
-
-(function($) {
-    $(function () {
-        $('.sub_menu').on('click', function (e) {
-            e.preventDefault();
-            var $subNav = $('.sub_menu_nav', this);
-            if ($subNav.css("display") === "none") {
-                $(this).addClass('is-active');
-                $subNav.velocity('slideDown', {duration: 400});
-            } else {
-                $(this).removeClass('is-active');
-                $subNav.velocity('slideUp', {duration: 400});
-            }
-        });
-    });
-})(jQuery);
-</script>
+(function(){
+  $("#menu_bar").click(function(){
+    $("#sidebar").slideToggle();
+    return false;
+  });
+  $(window).resize(function(){
+    var win = $(window).width();
+    var p = 480;
+    if(win > p){
+      $("#menu").show();
+    }
+  });
+});
