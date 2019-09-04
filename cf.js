d4a47a4126f20440e68c3176f12de136b9000df7
haramanami <manami.hara@cybermarketing>
2019-09-04 11:54:06 +0900

    fixed

--- cf.js
+++ cf.js
@@ -1,13 +1,6 @@
-(function(){
-  $("#menu_bar").click(function(){
-    $("#sidebar").slideToggle();
-    return false;
-  });
-  $(window).resize(function(){
-    var win = $(window).width();
-    var p = 480;
-    if(win > p){
-      $("#menu").show();
-    }
-  });
+function(){
+  $('#menu_bar').click(
+    function(){
+    $('.global_nav').show()
+    });
 });
